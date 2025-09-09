5eee6224fe167042549e3f418832e2413380e936
Nell-ok <nelli-nn@yandex.ru>
2025-09-09 12:21:28 +0300

    Добавляет компонент Button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
+import Button from "./components/Button.vue"
 const date = new Date().toLocaleDateString('ru-RU');
 </script>
 
 <template>
   <header>Текущая дата: {{ date }}.</header>
+  <main>
+    <Button>Начать игру</Button>
+  </main>
 </template>
 
 <style scoped>
